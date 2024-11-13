5ec1ebc14602e5ba142c9460cdfb2f699b12900f
th-sakib <mdthamidulhasansakib@gmail.com>
2024-11-14 00:10:37 +0600

    create redux store and fix minor bugs

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const UserDetails = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="flex justify-around items-center ">
+    <div className="flex flex-col justify-between items-center">
+      <div className="flex justify-around items-center w-[70vw] ">
         <h1 className="text-4xl">list of user details</h1>
         <button className="btn">Add new user</button>
       </div>
-      <ul className="list-disc">{/* <li>hi</li> */}</ul>
+      <ul className="list-disc">
+        <li>hi</li>
+      </ul>
 
       <div className="divider"></div>
       {/* <DeleteUser /> */}
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,12 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: "user",
-  initialState: [],
   reducers: {
     addUser(state, action) {},
-    removeUser(state, action) {},
+    deleteUser(state, action) {},
+    deleteUsers(state, action) {},
   },
 });
 
-export { userSlice };
+export default userSlice.reducer;
